df34a1e5d120bd687826025ebc12ec67272f2366
Deuris <denys.samofan@gmail.com>
2023-04-04 22:29:42 +0300

    New

---
+++ src/js/bundle.js
@@ -0,0 +1,23 @@
+//Properjs
+@@include('@@nodeRoot/node_modules/@popperjs/core/dist/umd/popper.min.js')
+
+// Bootstrap
+@@include('@@nodeRoot/node_modules/bootstrap/dist/js/bootstrap.min.js')
+
+// fsLightbox
+@@include('@@nodeRoot/node_modules/fslightbox/index.js')
+
+// Swiper
+@@include('@@nodeRoot/node_modules/swiper/swiper-bundle.min.js')
+
+// Simple-parallax
+@@include('@@nodeRoot/node_modules/simple-parallax-js/dist/simpleParallax.min.js')
+
+// AOS
+@@include('@@nodeRoot/node_modules/aos/dist/aos.js')
+
+// Pristine
+@@include('@@nodeRoot/node_modules/pristinejs/dist/pristine.js')
+
+// Functions
+@@include('@@vendorRoot/js/vendors/nioapp.js')
